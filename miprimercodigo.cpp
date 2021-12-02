b8b057082feec55dad211d803d19d21bc9b02fff
mcangrejo <74432387+mcangrejo@users.noreply.github.com>
2021-12-02 10:06:04 -0500

    Add files via upload

---
+++ miprimercodigo.cpp
@@ -0,0 +1,21 @@
+//Esto es un comentario
+/* Para hacer varias líneas de comentario
+se debe usar slash y asterisco */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Introduccion al lenguaje c");  //las instrucciones en c se terminan con ;
+	system("start http://www.samsung.com"); //abre pagina web en el explorador
+	system("start C:\\Windows\\System32\\Magnify.exe");
+	
+	
+	
+	return 0;	
+}
+
+
+
+
